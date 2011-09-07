8dcbadeb4d20646a33680723e3187103183d4fdc
Anurag Sing <anurag.s.1932@gmailcom>
2011-09-07 18:30:36 +1000

    first commit of connect.php

---
+++ connect.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * MySQL connection check
+ *
+ * Checks the connection to the local install of MySQL
+ * 
+ * @author Anurag Singh <s3237701@rmit.edu.au>
+ * @version 1.0
+ * @package Connect
+ */
+
+/**
+ * Hostname and port mysql is running on (can't use localhost)
+ */
+define('DB_HOST',   'yallara.cs.rmit.edu.au:51886');
+/**
+ * Name of database to connect to
+ */
+define('DB_NAME',   'winestore');
+/**
+ * Username to connect with
+ */
+define('DB_USER',   'winestore');
+/**
+ * Password to connect with
+ */
+define('DB_PW',     'annu2011');
+
+
+if (!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
+  echo 'Could not connect to mysql on ' . DB_HOST . "\n";
+  exit;
+}
+
+echo 'Connected to mysql on ' . DB_HOST . "<br/>\n";
+
+if (!mysql_select_db(DB_NAME, $dbconn)) {
+  echo 'Could not use database ' . DB_NAME . "\n";
+  echo mysql_error() . "\n";
+  exit;
+}
+
+echo 'Connected to database ' . DB_NAME . "<br/>\n";
+
+?>
